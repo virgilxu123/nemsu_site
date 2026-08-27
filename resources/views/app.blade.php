e8192a35868527a0794782dd33dec814278f1afd
jan-nemsu <jmdecastro.@nemsu.edu.ph>
2026-08-27 14:16:57 +0800

    update head title

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -30,8 +30,8 @@
             }
         </style>
 
-        <link rel="icon" href="/favicon.ico" sizes="any">
-        <link rel="icon" href="/favicon.svg" type="image/svg+xml">
+        {{-- <link rel="icon" href="/favicon.ico" sizes="any"> --}}
+        <link rel="icon" href="/favicon.png" type="image/svg+xml">
         <link rel="apple-touch-icon" href="/apple-touch-icon.png">
 
         <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -147,7 +147,7 @@
 
         </style>
         <x-inertia::head>
-            <title>{{ config('app.name', 'Laravel') }}</title>
+            <title>{{ config('app.name', 'NEMSU') }}</title>
         </x-inertia::head>
     </head>
     <body class="font-sans antialiased">
